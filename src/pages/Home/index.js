e5877bca53bce0e53c7d373b59a00cdf54a35390
dattmpc02133 <datgoodboy2002@gmail.com>
2022-10-29 21:38:18 +0700

    pul

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,12 +14,36 @@ function Home() {
     const slider = useRef();
     const settings = {
         dots: false,
-        infinite: true,
+        infinite: false,
         speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        slickPrev: true,
-        slickNext: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
     };
     return (
         <>
@@ -36,39 +60,40 @@ function Home() {
                     <div className={cx('slide-cate-product')}>
                         <div className={cx('slide-owl-stage')}>
                             <Slider ref={slider} {...settings}>
-                                <div className={cx('slide-olw-box')}>
-                                    {product.children.map((item, index) => (
-                                        <div className={cx('olw-item')} key={index}>
-                                            <a href="#" className={cx('olw-item-link')}>
-                                                <div className={cx('olw-newDiscount-head')}>
-                                                    <label>{item.news}</label>
-                                                    <div className={cx('olw-percent')}>{item.discount}</div>
-                                                </div>
-                                                <div className={cx('olw-images-box')}>
-                                                    <img
-                                                        src={item.image}
-                                                        alt={item.title}
-                                                        className={cx('olw-img-slide')}
-                                                    />
-                                                </div>
-                                                <h3>{item.title}</h3>
-                                                <span className={cx('price')}>
-                                                    {item.priceReducer} <del>{item.price}</del>
-                                                </span>
-                                            </a>
-                                        </div>
-                                    ))}
-                                </div>
+                                {product.children.map((item, index) => (
+                                    <div className={cx('olw-item')} key={index}>
+                                        <a href="#" className={cx('olw-item-link')}>
+                                            <div className={cx('olw-newDiscount-head')}>
+                                                <label>{item.news}</label>
+                                                <div className={cx('olw-percent')}>{item.discount}</div>
+                                            </div>
+                                            <div className={cx('olw-images-box')}>
+                                                <img
+                                                    src={item.image}
+                                                    alt={item.title}
+                                                    className={cx('olw-img-slide')}
+                                                />
+                                            </div>
+                                            <h3>{item.title}</h3>
+                                            <span className={cx('price')}>
+                                                {item.priceReducer} <del>{item.price}</del>
+                                            </span>
+                                        </a>
+                                    </div>
+                                ))}
+                                {/* <div className={cx('slide-olw-box')}>
+                                    
+                                </div> */}
                                 <div className={cx('olw-btn_nav')} style={{ display: 'none' }}></div>
                             </Slider>
-                            {/* <div className={cx('button-slide')}>
-                                <div className={cx('olw-btn-prev')} onClick={() => slider?.current?.slickPrev()}>
+                            <div className={cx('button-slide')}>
+                                <div className={cx('olw-btn-prev')}>
                                     <VscArrowLeft className={cx('icon-slide')} />
                                 </div>
                                 <div className={cx('olw-btn-next')}>
                                     <VscArrowRight className={cx('icon-slide')} />
                                 </div>
-                            </div> */}
+                            </div>
                         </div>
                     </div>
                 </div>
