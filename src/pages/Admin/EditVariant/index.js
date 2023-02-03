210e4fe6ea6996c03e44732bff310c4972d28491
honhuthuynh1 <huynh9a11617@gmail.com>
2023-02-03 20:05:21 +0700

    varian

--- src/pages/Admin/EditVariant/index.js
+++ src/pages/Admin/EditVariant/index.js
@@ -16,109 +16,109 @@ function EditVariant() {
     const [selectActive, setSelectActive] = useState();
 
     const params = useParams();
-    useEffect(() => {
-        const getById = async () => {
-            try {
-                const byIdBrand = await productApi.getByIdVariant(params.id);
-                // setBrandsId(byIdBrand.data.is_post);
-                console.log('byIdBrand', byIdBrand.data);
-                setNameVariant(byIdBrand.data.variant_name);
-                setSelectActive(byIdBrand.data.is_active);
-            } catch (error) {
-                console.log('Lỗi thương hiệu theo id', error);
-            }
-        };
-        getById();
-    }, []);
+        useEffect(() => {
+            const getById = async () => {
+                try {
+                    const byIdBrand = await productApi.getByIdVariant(params.id);
+                    // setBrandsId(byIdBrand.data.is_post);
+                    console.log('byIdBrand', byIdBrand.data);
+                    setNameVariant(byIdBrand.data.variant_name);
+                    setSelectActive(byIdBrand.data.is_active);
+                } catch (error) {
+                    console.log('Lỗi thương hiệu theo id', error);
+                }
+            };
+            getById();
+        }, []);
 
-    const handleSubmit = (e) => {
-        setLoading(true);
-        e.preventDefault();
-        const data = { variant_name: nameVariant, is_active: selectActive }; // , is_post: brandsId
-        // console.log('data', data);
-        const EditVariant = async () => {
-            try {
-                const result = await productApi.updateVariants(data, params.id);
-                setMessStatus(result.message);
-                setStatusHandle(true);
-                setModal(true);
-                setLoading(false);
-            } catch (error) {
-                console.log('Failed to Edit: ', error);
-                const res = error.response.data;
-                setMessStatus(res.message);
-                setLoading(false);
-                setModal(true);
-                setStatusHandle(false);
-            }
-        };
-        EditVariant();
-    };
-    // const handleChangeSelections = (id) => {
-    //     setBrandsId(id);
-    // };
-    return (
-        <div className="wrapper">
-            {loading ? <Loading /> : ''}
-            {modal && <Modal closeModal={setModal} message={messStatus} status={statusHandle} />}
-            <div className="content__heading">
-                <h2 className="content__heading--title">Cập nhật thương hiệu</h2>
-                <p className="content__heading--subtitle">Cập nhật thương hiệu</p>
-            </div>
+            const handleSubmit = (e) => {
+                setLoading(true);
+                e.preventDefault();
+                const data = { variant_name: nameVariant, is_active: selectActive }; // , is_post: brandsId
+                // console.log('data', data);
+                const EditVariant = async () => {
+                    try {
+                        const result = await productApi.updateVariants(data, params.id);
+                        setMessStatus(result.message);
+                        setStatusHandle(true);
+                        setModal(true);
+                        setLoading(false);
+                    } catch (error) {
+                        console.log('Failed to Edit: ', error);
+                        const res = error.response.data;
+                        setMessStatus(res.message);
+                        setLoading(false);
+                        setModal(true);
+                        setStatusHandle(false);
+                    }
+                };
+                EditVariant();
+            };
+        // const handleChangeSelections = (id) => {
+        //     setBrandsId(id);
+        // };
+        return (
+            <div className="wrapper">
+                {loading ? <Loading /> : ''}
+                {modal && <Modal closeModal={setModal} message={messStatus} status={statusHandle} />}
+                <div className="content__heading">
+                    <h2 className="content__heading--title">Cập nhật thương hiệu</h2>
+                    <p className="content__heading--subtitle">Cập nhật thương hiệu</p>
+                </div>
 
-            <div className="content__wrapper">
-                <div className="content__main">
-                    <form className="form__content" onSubmit={(e) => handleSubmit(e)}>
-                        <div className="input__group">
-                            <div className="input__label">
-                                <label htmlFor="ip-name">Tên danh mục thương hiệu </label>
-                            </div>
-                            <div className="input__text">
-                                <input
-                                    value={nameVariant}
-                                    id="ip-name"
-                                    type="text"
-                                    className="input__text--ctrl"
-                                    placeholder="Tên danh muc"
-                                    required
-                                    onChange={(e) => setNameVariant(e.target.value)}
-                                />
-                            </div>
-                        </div>
-
-                        <div className="input__group">
-                            <div className="input__label">
-                                <label htmlFor="ip-name">Trạng thái</label>
-                            </div>
-                            <div className="input__text">
-                                <select
-                                    value={selectActive}
-                                    onChange={(e) => setSelectActive(e.target.value)}
-                                    className="input__text--ctrl"
-                                >
-                                    <option value="">Chọn trạng thái</option>
-                                    <option value="0">Chưa kích hoạt</option>
-                                    <option value="1">Đang kích hoạt</option>
-                                </select>
-                            </div>
-                        </div>
-
-                        {message && typeof message == 'string' ? (
+                <div className="content__wrapper">
+                    <div className="content__main">
+                        <form className="form__content" onSubmit={(e) => handleSubmit(e)}>
                             <div className="input__group">
-                                <span className={('input__group--mess', 'suscess')}>{message}</span>
+                                <div className="input__label">
+                                    <label htmlFor="ip-name">Tên danh mục thương hiệu </label>
+                                </div>
+                                <div className="input__text">
+                                    <input
+                                        value={nameVariant}
+                                        id="ip-name"
+                                        type="text"
+                                        className="input__text--ctrl"
+                                        placeholder="Tên danh muc"
+                                        required
+                                        onChange={(e) => setNameVariant(e.target.value)}
+                                    />
+                                </div>
                             </div>
-                        ) : (
-                            false
-                        )}
 
-                        <div className="btn__form">
-                            <button className="btn__form--ctrl">Cập nhật thương hiệu</button>
-                        </div>
-                    </form>
+                            <div className="input__group">
+                                <div className="input__label">
+                                    <label htmlFor="ip-name">Trạng thái</label>
+                                </div>
+                                <div className="input__text">
+                                    <select
+                                        value={selectActive}
+                                        onChange={(e) => setSelectActive(e.target.value)}
+                                        className="input__text--ctrl"
+                                    >
+                                        <option value=""> Chọn trạng thái</option>
+                                        <option value="0"> Chưa kích hoạt</option>
+                                        <option value="1"> Đang kích hoạt</option>
+                                    </select> 
+                                </div>
+                            </div>
+
+                            {message && typeof message == 'string' ? (
+                                <div className="input__group">
+                                    <span className={('input__group--mess', 'suscess')}>{message}</span>
+                                </div>
+                            ) : (
+                                false
+                            )}
+
+                            <div className="btn__form">
+                                <button className="btn__form--ctrl"> Cập nhật thương hiệu</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-}
+        );
+    }
 
 export default EditVariant;
--- src/pages/Admin/ListVariant/index.js
+++ src/pages/Admin/ListVariant/index.js
@@ -7,118 +7,119 @@ import brandApi from '~/api/brandApi';
 import Dialog from '~/components/Dialog';
 import Pagination from '~/components/Pagination';
 import productApi from '~/api/productApi';
-function ListVariants() {
-    const [listVariants, setListVariants] = useState();
-    const [loading, setLoading] = useState(false);
-    const [message, setMessage] = useState();
-    const [messStatus, setMessStatus] = useState();
-    const [statusHandle, setStatusHandle] = useState();
-    const [modal, setModal] = useState(false);
-    const [deleteBrand, SetDeleteBrand] = useState();
-    const [comfirm, setComfirm] = useState(false);
-    const [page, setPage] = useState(1);
-    const [pageBrand, setPageBrand] = useState([]);
-    const deleteBrands = useRef();
 
-    const handleDelete = (id) => {
-        setComfirm(true);
-        deleteBrands.current = id;
-    };
-    const getAllVariant = async () => {
-        try {
-            const allBrands = await productApi.getVariants();
-            console.log('allBrands', allBrands);
-            setListVariants(allBrands.data);
-            console.log(allBrands);
-            setPageBrand(allBrands.paginator);
-        } catch (error) {
-            console.log('lỗi lấy danh sách', error);
-        }
-    };
-    useEffect(() => {
-        getAllVariant();
-    }, []);
-    const handleAction = (type) => {
-        if (type) {
-            setComfirm(false);
-            const getDeletBrand = async () => {
-                setLoading(true);
-                try {
-                    const deleteBrand = await productApi.deleteVariants(deleteBrands.current);
-                    setMessStatus(deleteBrand.message);
-                    getAllVariant();
-                    setStatusHandle(true);
-                    setModal(true);
-                    setLoading(false);
-                } catch (error) {
-                    console.log('lỗi khi xóa', error);
-                    const res = error.response.data;
-                    setMessStatus(res.message);
-                    setStatusHandle(false);
-                    setModal(true);
-                    setLoading(false);
-                }
-            };
-            getDeletBrand();
-        }
-    };
+    function ListVariants() {
+        const [listVariants, setListVariants] = useState();
+        const [loading, setLoading] = useState(false);
+        const [message, setMessage] = useState();
+        const [messStatus, setMessStatus] = useState();
+        const [statusHandle, setStatusHandle] = useState();
+        const [modal, setModal] = useState(false);
+        const [deleteBrand, SetDeleteBrand] = useState();
+        const [comfirm, setComfirm] = useState(false);
+        const [page, setPage] = useState(1);
+        const [pageBrand, setPageBrand] = useState([]);
+        const deleteBrands = useRef();
 
-    return (
-        <div className="wrapper">
-            {loading ? <Loading /> : ''}
-            {comfirm && <Dialog closeDialog={setComfirm} action={handleAction} />}
-            {modal && <Modal closeModal={setModal} message={messStatus} status={statusHandle} />}
-            <div className="content__heading">
-                <h2 className="content__heading--title">Danh sách danh mục thương hiệu</h2>
-                <p className="content__heading--subtitle">Danh mục thương hiệu</p>
-            </div>
-            <div className="content__wrapper">
-                <div className="content__main">
-                    <div className="table__block">
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Tên thương hiệu</th>
-                                    <th>Trạng thái</th>
-                                    <th colSpan="2" className="text-center">
-                                        Thao tác
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {Array.isArray(listVariants)
-                                    ? listVariants.map((item, index) => (
-                                          <tr key={item.id}>
-                                              <td>{index + 1}</td>
-                                              <td>{item.variant_name}</td>
-                                              <td className={item.is_active == 1 ? 'active' : 'amountActive'}>
-                                                  {item.is_active == 1 ? 'Đang kích hoạt' : 'Chưa kích hoạt'}
-                                              </td>
-                                              <td className="text-center">
-                                                  <Link to={`/admin/variant/edit/${item?.id}`} state={{ item }}>
-                                                      Sửa
-                                                  </Link>
-                                              </td>
-                                              <td className="text-center">
-                                                  <Link
-                                                      onClick={() => {
-                                                          handleDelete(item.id);
-                                                      }}
-                                                  >
-                                                      Xóa
-                                                  </Link>
-                                              </td>
-                                          </tr>
-                                      ))
-                                    : false}
-                            </tbody>
-                        </table>
+        const handleDelete = (id) => {
+            setComfirm(true);
+            deleteBrands.current = id;
+        };
+        const getAllVariant = async () => {
+            try {
+                const allBrands = await productApi.getVariants();
+                console.log('allBrands', allBrands);
+                setListVariants(allBrands.data);
+                console.log(allBrands);
+                setPageBrand(allBrands.paginator);
+            } catch (error) {
+                console.log('lỗi lấy danh sách', error);
+            }
+        };
+        useEffect(() => {
+            getAllVariant();
+        }, []);
+        const handleAction = (type) => {
+            if (type) {
+                setComfirm(false);
+                const getDeletBrand = async () => {
+                    setLoading(true);
+                    try {
+                        const deleteBrand = await productApi.deleteVariants(deleteBrands.current);
+                        setMessStatus(deleteBrand.message);
+                        getAllVariant();
+                        setStatusHandle(true);
+                        setModal(true);
+                        setLoading(false);
+                    } catch (error) {
+                        console.log('lỗi khi xóa', error);
+                        const res = error.response.data;
+                        setMessStatus(res.message);
+                        setStatusHandle(false);
+                        setModal(true);
+                        setLoading(false);
+                    }
+                };
+                getDeletBrand();
+            }
+        };
+
+        return (
+            <div className="wrapper">
+                {loading ? <Loading /> : ''}
+                {comfirm && <Dialog closeDialog={setComfirm} action={handleAction} />}
+                {modal && <Modal closeModal={setModal} message={messStatus} status={statusHandle} />}
+                <div className="content__heading">
+                    <h2 className="content__heading--title">Danh sách danh mục thương hiệu</h2>
+                    <p className="content__heading--subtitle">Danh mục thương hiệu</p>
+                </div>
+                <div className="content__wrapper">
+                    <div className="content__main">
+                        <div className="table__block">
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Tên thương hiệu</th>
+                                        <th>Trạng thái</th>
+                                        <th colSpan="2" className="text-center">
+                                            Thao tác
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {Array.isArray(listVariants)
+                                        ? listVariants.map((item, index) => (
+                                            <tr key={item.id}>
+                                                <td>{index + 1}</td>
+                                                <td>{item.variant_name}</td>
+                                                <td className={item.is_active == 1 ? 'active' : 'amountActive'}>
+                                                    {item.is_active == 1 ? 'Đang kích hoạt' : 'Chưa kích hoạt'}
+                                                </td>
+                                                <td className="text-center">
+                                                    <Link to={`/admin/variant/edit/${item?.id}`} state={{ item }}>
+                                                        Sửa
+                                                    </Link>
+                                                </td>
+                                                <td className="text-center">
+                                                    <Link
+                                                        onClick={() => {
+                                                            handleDelete(item.id);
+                                                        }}
+                                                    >
+                                                        Xóa
+                                                    </Link>
+                                                </td>
+                                            </tr>
+                                        ))
+                                        : false}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
-}
+        );
+    }
 
-export default ListVariants;
+    export default ListVariants;
